4af956303b404b19cf7c15d73df5af13abc8d70d
jacartuyvels <jacques.cartuyvels@student.uclouvain.be>
2017-12-14 14:03:46 +0100

    lol

--- marginLin.m
+++ marginLin.m
@@ -1,4 +1,4 @@
-function [h, c, tau] = marginsep(data0, data1)
+function [h, c, tau] = marginLin(data0, data1)
 % Find a maximum margin separating hyperplane betweens sets of points pa et pb
 % (data points are arranged as columns in matrices pa and pb)
 % Model: min t s.t. h^T ai + c <= -1, h^t bi + c >= 1 and norm(h) <= t
@@ -9,7 +9,7 @@ nA = size(data0,2);
 nB = size(data1,2);
 n = size(data0,1);
 
-A = [ data0' ones(nA,2); -data1' -ones(nB,1) ones(nB,1); zeros(1,n+2); eye(n) zeros(n,2)]';
+A = [-data0' -ones(nA,1) ones(nA,1);data1' ones(nB,2); zeros(1,n+2); eye(n) zeros(n,2)]';
 c = [zeros(nA+nB,1); 1; zeros(n,1)];
 b = [zeros(n+1,1); 1];
 K.l = nA+nB;
--- run2.m
+++ run2.m
@@ -1,7 +1,7 @@
 %% Homework 3 optimization
 %% Exercise 2
 
-ata = load('Homework-3-data.mat');
+data = load('Homework-3-data.mat');
 
 training = data.training;
 test = data.test;
@@ -12,10 +12,40 @@ label = training.labels;
 image = training.images;
 shape = size(image);
 
+
 image = reshape(image(:,:,:),[shape(1)*shape(2), shape(3)]);
 [data0 data1] = partition(image,label,0);
 
-data0 = data0(:,1:1000);
-data1 = data1(:,1:6000);
+label2 = test.labels;
+image2 = test.images;
+shape2 = size(image2);
 
-[h,c,tau]=marginLin(data0, data1);
+
+image2 = reshape(image2(:,:,:),[shape2(1)*shape2(2), shape2(3)]);
+
+
+data0 = data0(:,1:2000);
+data1 = data1(:,1:12000);
+
+[h,c]=marginLin(data0,data1,10);
+
+[data0 data1] = partition(image2,label2);
+
+y1 = data0'*h +c
+y2 = data1'*h + c
+accuracy1 = 0
+accuracy2 = 0
+for i = 1:size(data0,2)
+    accuracy1 = accuracy1 + max(0,y1(i))/y1(i);
+end
+accuracy1 = accuracy1/(size(data0,2))
+ for i = 1:size(data1,2)
+    accuracy2 = accuracy2 + min(0,y2(i))/y2(i);
+end
+accuracy2 = accuracy2/(size(data1,2))
+
+
+pause;
+
+
+    
